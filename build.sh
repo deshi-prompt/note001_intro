a7fafc7a0c653316a54e8e87469c9e58025add8f
kasai <takara.kasai@gmail.com>
2016-01-24 19:12:05 +0900

    make src and template dir.

--- build.sh
+++ build.sh
@@ -7,21 +7,32 @@ echo $?
 #  popd
 #fi
 
+pushd image
+bash convert.sh
+popd
+
+SRC_DIR=`pwd`/src
+TEMPLATE_DIR=`pwd`/template
+BUILD_DIR=`pwd`/build
+
 # html形式で貼り付けている画像をmarkdown形式に変換する
-cat 2015_winter.md | sed -e "s/^<\/*div.\+//g" | sed -e "s/^<br.\+//g"  | sed -e "s/^<img src=\"\([a-zA-Z0-9_\.\/]\+\)\".\+<br>\(.\+\)<br>/![\2](\1)/g" > tmp.md
-cat tmp.md | sed -e "s/jpg/eps/g" | sed -e "s/png/eps/g" | sed -e "s/JPG/eps/g" > tmp2.md
+cat ${SRC_DIR}/2015_winter.md | sed -e "s/^<\/*div.\+//g" | sed -e "s/^<br.\+//g"  | sed -e "s/^<img src=\"\([a-zA-Z0-9_\.\/]\+\)\".\+<br>\(.\+\)<br>/![\2](\1)/g" > ${BUILD_DIR}/tmp.md
+cat ${BUILD_DIR}/tmp.md | sed -e "s/jpg/eps/g" | sed -e "s/png/eps/g" | sed -e "s/JPG/eps/g" > ${BUILD_DIR}/tmp2.md
 
-cat tex_header.tex > 2015_winter.tex 
+mkdir -p ${BUILD_DIR}
+
+cat ${TEMPLATE_DIR}/tex_header.tex > ${BUILD_DIR}/2015_winter.tex 
 #pandoc -t latex 2015_winter.md >> 2015_winter.tex
-pandoc -t latex tmp2.md >> 2015_winter.tex
-cat tex_bibitem.tex >> 2015_winter.tex
-pandoc -t latex 2015_winter_afterword.md >> 2015_winter.tex
-cat tex_hooter.tex >> 2015_winter.tex 
+pandoc -t latex ${BUILD_DIR}/tmp2.md >> ${BUILD_DIR}/2015_winter.tex
+cat ${TEMPLATE_DIR}/tex_bibitem.tex >> ${BUILD_DIR}/2015_winter.tex
+pandoc -t latex ${SRC_DIR}/2015_winter_afterword.md >> ${BUILD_DIR}/2015_winter.tex
+cat ${TEMPLATE_DIR}/tex_hooter.tex >> ${BUILD_DIR}/2015_winter.tex 
 
-cat 2015_winter.tex | sed -e "s/htbp/H/g" > tmp.tex
+cat ${BUILD_DIR}/2015_winter.tex | sed -e "s/htbp/H/g" > ${BUILD_DIR}/tmp.tex
 
-platex -kanji=utf8 tmp.tex 
-dvipdf tmp.dvi
-cp tmp.pdf 2015_winter.pdf
-gnome-open 2015_winter.pdf
+platex -kanji=utf8 -output-directory=${BUILD_DIR} ${BUILD_DIR}/tmp.tex 
+dvipdf ${BUILD_DIR}/tmp.dvi ${BUILD_DIR}/2015_winter.pdf
+#cp tmp.pdf 2015_winter.pdf
+
+gnome-open ${BUILD_DIR}/2015_winter.pdf
 
